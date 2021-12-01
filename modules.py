ab20236b053ed64ca2b663725bba04082420eafd
konek2112 <konek2112@gmail.com>
2021-12-01 16:33:11 +0300

    modified test.py

--- modules.py
+++ modules.py
@@ -1,3 +1,2 @@
 def raw_char(input_file):
     return (len(input_file))
-print(raw_char('input_file'))
--- test.py
+++ test.py
@@ -2,18 +2,3 @@ print("Hello World!")
 print('хай бич!')
 print('ess)))')
 print("Gitignore working")
-
-# проблема в скорости и также что при большом тексте не направильно считает
-def count_words():
-    line = input()
-    i = 0
-    for space in line:
-        if space == " ":
-            i += 1
-        else:
-            continue
-    ans = i + 1
-    return(ans)
-
-
-count_words()
