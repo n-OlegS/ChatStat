69fe63073ee4e4249e38ca4641e2cf52c61c7dc3
Oleg Skvortsov <oleg@73rus.com>
2021-12-01 10:40:08 +0300

    Updated

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from modules import *
+
+path = str(input("Enter path to text file: "))
+c = 0
+
+try:
+    file = open(path, 'r')
+except FileNotFoundError:
+    print("File not found.")
+    quit()
+
+lines = file.readlines()
+
+for i in lines:
+    c += 1
+
+print(c)
