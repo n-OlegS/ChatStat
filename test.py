45ba487887c12d2092bd435f6618e468f9d57aec
konek2112 <konek2112@gmail.com>
2021-12-01 16:15:27 +0300

    modified test.py

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 print("Hello World!")
 print('хай бич!')
-<<<<<<< HEAD
 print('ess)))')
-=======
 print("Gitignore working")
->>>>>>> 876812353c3a60537cac0ca42d8b8a824046190a
+
+# проблема в скорости и также что при большом тексте не направильно считает
+def count_words():
+    line = input()
+    i = 0
+    for space in line:
+        if space == " ":
+            i += 1
+        else:
+            continue
+    ans = i + 1
+    return(ans)
+
+
+count_words()
