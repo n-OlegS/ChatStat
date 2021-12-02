5369f669f9982ce030846ed4b448b3e468f9bba8
konek2112 <konek2112@gmail.com>
2021-12-02 10:39:17 +0300

    Added word count

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
-# немного говно кода
-a = str(input("Enter path to text file: "))
+def count_words(path):
+    a = open(path, 'r')
+    text = a.read()
+    return text.count(' ') + 1
+    a.close()
 
-def count_words():
-    chat = open('chat1.txt', 'r')
-
-    count = 0
-    for line in chat:
-        count = count + 1
-    print('Number of lines in the file:', count)
-
-    chat.close()
-
-
-count_words()
-
-# счетчик пробелов в строке
-# i = 0
-# line = chat.readline()
-# i = line.count(' ')
-# print(i + 1)
+print(count_words('/Users/ula/PycharmProjects/Projectneolegs/ChatStat/clean.txt'))
